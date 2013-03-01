9024d152eaef59481de89b2110cfecf4620bc197
zhservices <github@zhservices.com>
2013-03-01 16:01:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<div style="font-size:14px;font-weight:bold;text-align:center;width:100%">
-<?php
-echo "ZH HealthCare";
-?>
+<div style="font-size:55px;font-weight:bold;text-align:center;width:100%;font-family:Verdana;margin-top:28%">
+<span style="color:#043752">ZH</span> <span style="color:#498AC2">HEALTHCARE</span>
 </div>
